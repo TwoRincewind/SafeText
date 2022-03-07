64ec3aafad1f2a235b47f1a9d8c3b8348a40b62f
Fedor <68068200+DvaRincewind@users.noreply.github.com>
2022-03-07 20:14:57 +0700

    Init
    Первая версия, рабочий некрасивый интерфейс + начинка

---
+++ safe_text.js
@@ -0,0 +1,36 @@
+function task(table, oldText, key, flag) {
+    newText = "";
+    for (index = 0; index < oldText.length; index++) {
+        oldTextChar = oldText[index];
+        keyChar = key[index % key.length];
+        oldTextCharIndex = table.indexOf(oldTextChar);
+        keyCharIndex = table.indexOf(keyChar);
+        newTextCharIndex = (oldTextCharIndex + keyCharIndex * (flag ? 1 : -1) + table.length) % table.length;
+        newTextChar = table[newTextCharIndex];
+        newText += newTextChar;
+    }
+    return newText;
+}
+
+document.checking.button.addEventListener("click", execute);
+document.checking.flag.addEventListener("click", flag_check);
+
+function flag_check() {
+    document.checking.button.value = (document.checking.flag.checked ? "Зашифровать" : "Расшифровать");
+}
+
+function execute() {
+    var input_table = String(document.table.table.value);
+    var input_text = String(document.input.textInput.value);
+    var input_key = String(document.checking.key.value);
+    var input_flag = Boolean(document.checking.flag.checked);
+    for (i = 0; i < input_text.length; i++) {
+        if (!input_table.includes(input_text[i])) {
+            alert("Похоже, что текст содержит символы, которых нет в таблице...");
+            return;
+        }
+    }
+    document.output.textOutput.value = task(input_table, input_text, input_key, input_flag);
+}
+
+flag_check();
