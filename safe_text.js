7fd001fb0442e0f7e375bb98514cb6cf2e7fade4
Fedor <theodor.popov.2007@gmail.com>
2022-03-08 06:44:49 +0000

    fix multi-warning

--- safe_text.js
+++ safe_text.js
@@ -24,13 +24,13 @@ function execute() {
     var input_text = String(document.input.textInput.value);
     var input_key = String(document.checking.key.value);
     var input_flag = Boolean(document.checking.flag.checked);
-    var exec_flag = true;
+    var exec_flag = text_flag = key_flag = true;
     if (input_table.length == 0) {
         document.table.table.style.borderColor = "red";
         alert("Введите таблицу!");
         exec_flag = false;
     } else {
-        document.table.table.style.borderColor = "black";
+        document.table.table.style.borderColor = "green";
     }
     {
         if (input_text.length == 0) {
@@ -38,12 +38,14 @@ function execute() {
             alert("Введите текст!");
             exec_flag = false;
         } else {
-            document.input.textInput.style.borderColor = "black";
+            document.input.textInput.style.borderColor = "green";
         }
         for (i = 0; i < input_text.length; i++) {
             if (input_text[i] != "\n" && !input_table.includes(input_text[i])) {
-                alert("Похоже, что текст содержит символы, которых нет в таблице...");
-                exec_flag = false;
+                if (text_flag) {
+                    alert("Похоже, что текст содержит символы, которых нет в таблице...");
+                    text_flag = false;
+                }
             }
         }
     }
@@ -53,16 +55,18 @@ function execute() {
             alert("Введите ключ!");
             exec_flag = false;
         } else {
-            document.checking.key.style.borderColor = "black";
+            document.checking.key.style.borderColor = "green";
         }
         for (i = 0; i < input_key.length; i++) {
             if (input_key[i] != "\n" && !input_table.includes(input_key[i])) {
-                alert("Похоже, что ключ содержит символы, которых нет в таблице...");
-                exec_flag = false;
+                if (key_flag) {
+                    alert("Похоже, что ключ содержит символы, которых нет в таблице...");
+                    key_flag = false;
+                }
             }
         }
     }
-    if (exec_flag) {
+    if (exec_flag && text_flag && key_flag) {
         var input_list = input_text.split("\n");
         document.output.textOutput.value = "";
         for (i = 0; i < input_list.length; i++) {
